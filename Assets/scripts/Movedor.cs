e5c736e9d974165176d8248b3082e7cdf3772af2
otreblan <otreblain@gmail.com>
2018-05-29 14:04:15 -0500

    versión 0.1.012

---
+++ Assets/scripts/Movedor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movedor : MonoBehaviour {
+	RectTransform tra;
+	int senti;
+	System.Random random;
+
+	// Use this for initialization
+	void Start () {
+		tra = this.GetComponent<RectTransform>();
+		random = new System.Random();
+		switch(random.Next(2)){
+			case 0:
+				senti = 10;
+				break;
+			case 1:
+				senti = -10;
+				break;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		tra.Rotate(new Vector3(0.0f, 0.0f, Time.deltaTime*senti));
+	}
+}
--- README.md
+++ README.md
@@ -3,3 +3,7 @@ a
 Botón del randomnes random aleatorio
 
 IDEAS:
+Hacer un fondo en el que los símbolos reboten con las paredes y giren aleatoriamente.<Alberto>
+Usar firebase para guardar los memes y videos random.<Alberto>
+Mejorar el fondo y los sprites del botón.<Alberto>
+
